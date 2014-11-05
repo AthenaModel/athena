ff7d0d142517f772e8fcd5d8856291b6c5554ced
Dave Hanks <David.R.Hanks@jpl.nasa.gov>
2014-11-05 07:17:43 -0800

    Updated for build 6.3.0a2

--- lib/projectlib/pkgModules.tcl
+++ lib/projectlib/pkgModules.tcl
@@ -35,8 +35,8 @@ package require tls 1.6
 package require tdom 0.8
 package require struct::set 2.2
 package require kiteutils 0.4.3
-package require marsutil 3.0.2
-package require simlib 3.0.2
+package require marsutil 3.0.3
+package require simlib 3.0.3
 # -kite-require-end
 
 package require kiteinfo
