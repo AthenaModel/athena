aa26da88c03f4681fbc82db074cc675737537ff9
Dave Hanks <David.R.Hanks@jpl.nasa.gov>
2014-11-19 10:29:49 -0800

    Revert "Revert "Updated working version""
    This reverts commit 9b6c0722283dda083c7d5f3fdc74927be400dc73.

--- lib/projectlib/pkgModules.tcl
+++ lib/projectlib/pkgModules.tcl
@@ -15,7 +15,7 @@
 # Package Definition
 
 # -kite-provide-start  DO NOT EDIT THIS BLOCK BY HAND
-package provide projectlib 6.3.0a3
+package provide projectlib 6.3.0a4
 # -kite-provide-end
 
 #-----------------------------------------------------------------------
