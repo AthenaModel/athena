79678c29c1cab9660f12b926c78264b341a0a91a
Dave Hanks <David.R.Hanks@jpl.nasa.gov>
2014-12-11 11:07:10 -0800

    SERVICE tactic refactor
    The service tactic has been refactored to include a mode variable that
    allows more flexibility.  In addition to setting LOS to a specific
    amount, LOS can be set to the required level, expected level or to a +/-
    percentage of the current actual level.
    
    Note that setting the LOS to the saturation level can be achieved simply
    by setting the LOS to 1.0

--- lib/app_athena/shared/tactic_service.tcl
+++ lib/app_athena/shared/tactic_service.tcl
@@ -21,6 +21,8 @@ tactic define SERVICE "Update Level of Service" {system actor} -onlock {
 
     # Editable Parameters
     variable s      ;# The abstract service to change ALOS
+    variable mode   ;# One of: EXACT, REQ, EXPECT or DELTA
+    variable deltap ;# Delta pct of ALOS up or down when mode is DELTA
     variable nlist  ;# A gofer::NBHOODS value
     variable los    ;# Actual level of service
 
@@ -34,6 +36,8 @@ tactic define SERVICE "Update Level of Service" {system actor} -onlock {
         # Initialize state variables
         set nlist  [gofer::NBHOODS blank]
         set los    1.0
+        set deltap 0.0
+        set mode   EXACT
         set s      ENERGY
 
         # Initial state is invalid (empty nlist)
@@ -66,14 +70,40 @@ tactic define SERVICE "Update Level of Service" {system actor} -onlock {
         # FIRST, get the owner and build the narrative appropriately
         set owner [my agent]
         set narr "Actor {actor:$owner} "
-        set pct [format %.1f%% [expr {$los * 100.0}]]
+        set pct  [format %.1f%% [expr {$los * 100.0}]]
+        set mult [expr {$deltap >= 0.0 ? 1.0 : -1.0}]
+        set pdel [format %.1f%% [expr {$deltap * $mult}]]
 
         if {$owner eq "SYSTEM"} {
             set narr "The SYSTEM agent "
         }
 
-        append narr "sets the actual level of $s service to "
-        append narr "$pct of saturation level in "
+        append narr "sets the actual level of $s service "
+
+        switch -exact -- $mode {
+            EXACT {
+                append narr "to $pct of saturation level in "
+            }
+
+            REQ {
+                append narr "to the required LOS in "
+            }
+
+            EXPECT {
+                append narr "to the expected LOS in "
+            }
+
+            DELTA {
+                if {$deltap >= 0.0} {
+                    set dir "up"
+                } else {
+                    set dir "down"
+                }
+                append narr "$dir by $pdel of current actual level in "
+            }
+
+            default {error "Unknown mode: \"$mode\""}
+        }
 
         append narr [gofer::NBHOODS narrative $nlist]
         
@@ -94,8 +124,51 @@ tactic define SERVICE "Update Level of Service" {system actor} -onlock {
 
         sigevent log 2 tactic $msg {*}$objects
 
+        set glist [list]
+        foreach n $nbhoods {
+            lappend glist {*}[civgroup gIn $n]
+        }
+
+        set gclause "g IN ('[join $glist {','}]') AND s='$s'"
+
         # NEXT, update actual LOS in the nbhoods specified
-        service actual $nbhoods $s $los
+        switch -exact -- $mode {
+            EXACT {
+                # All nbhoods receive the same LOS
+                service actual $nbhoods $s $los
+            }
+            
+            REQ {
+                # Set LOS to the required level by groups in nbhoods
+                rdb eval "
+                    UPDATE service_sg
+                    SET new_actual = required
+                    WHERE $gclause
+                "
+            }
+
+            EXPECT {
+                # Set LOS to the expected level by groups in nbhoods
+                rdb eval "
+                    UPDATE service_sg
+                    SET new_actual = expected
+                    WHERE $gclause
+                "
+            }
+
+            DELTA {
+                # Set LOS up or down by a percentage of current ALOS
+                # by groups in nbhoods
+                let frac {$deltap / 100.0}
+                rdb eval "
+                    UPDATE service_sg
+                    SET new_actual = max(0.0,min(1.0,actual + (actual * $frac)))
+                    WHERE $gclause
+                "
+            }
+
+            default {error "Unknown mode: \"$mode\""}
+        }
     }
 }
 
@@ -112,14 +185,30 @@ order define TACTIC:SERVICE {
         text tactic_id -context yes \
             -loadcmd {beanload}
 
-        rcc "Neighborhoods:" -for nlist
+        rcc "Neighborhoods:" -for nlist -span 3
         gofer nlist -typename gofer::NBHOODS
 
-        rcc "Service Type:" -for s
+        rcc "Service:" -for s 
         enum s -listcmd {eabservice names} -defvalue ENERGY
 
-        rcc "ALOS:" -for los
-        frac los
+        rcc "Level:" -for mode
+        selector mode {
+            case EXACT "Exactly this LOS" {
+                cc "" -for los
+                frac los
+            }
+
+            case REQ "Set to Required LOS" {}
+
+            case EXPECT "Set to Expected LOS" {}
+
+            case DELTA "Change by (+/-)" {
+                cc "" -for deltap 
+                text deltap
+                c  
+                label "% of ALOS"
+            }
+        }
     }
 
 } {
@@ -132,14 +221,16 @@ order define TACTIC:SERVICE {
     # Validation of initially invalid items or contingent items
     # takes place on sanity check.
     prepare nlist     
-    prepare s        -toupper -type eabservice
-    prepare los -num          -type rfraction
+    prepare s      -toupper -type eabservice
+    prepare mode   -toupper -selector
+    prepare deltap          -type rpercentpm
+    prepare los    -num     -type rfraction
 
     returnOnError -final
 
     # NEXT, modify the tactic
     setundo [$tactic update_ {
-        nlist s los
+        nlist s los mode deltap
     } [array get parms]]
 }
 
