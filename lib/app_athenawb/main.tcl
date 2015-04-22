59ae74f18e893aa44de66e02999275eff51c9e1c
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2015-04-22 14:49:19 -0700

    Fixes #166
    File was left out of previous #166 commit.

--- lib/app_athenawb/main.tcl
+++ lib/app_athenawb/main.tcl
@@ -32,29 +32,3 @@ proc main {argv} {
     app init $argv
 }
 
-# errexit line...
-#
-# line   -  One or more lines of text, as distinct arguments.
-#
-# On Linux/OS X or when Tk is not loaded, writes the text to standard 
-# output, and exits. On Windows, pops up a messagebox and exits when 
-# the box is closed.
-
-proc errexit {args} {
-    set text [join $args \n]
-
-    set f [open "error.log" w]
-    puts $f $text
-    close $f
-
-    if {[os type] ne "win32" || !$::tkLoaded} {
-         puts $text
-    } else {
-        wm withdraw .
-        modaltextwin popup \
-            -title   "Athena is shutting down" \
-            -message $text
-    }
-
-    exit 1
-}
