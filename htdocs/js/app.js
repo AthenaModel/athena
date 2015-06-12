5fe1a0ed0feb74667ba6bbda103b786e76c0cfc3
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2015-06-12 08:16:14 -0700

    Angular web app: Initial main navbar
    Replaced the existing navbar with one using an angular controller to
    display the write content.

--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -9,6 +9,16 @@ app.controller('ArachneController', function() {
     this.version = 'v6.X.YaZ';
 });
 
+app.controller('MainTabController', function() {
+    this.tab = 'home';
+    this.setTab = function(which) {
+        this.tab = which;
+    };
+    this.isSet = function(tab) {
+        return this.tab === tab;
+    };
+});
+
 app.controller('ScenarioListController', ['$http', function($http) {
     var my = this;
     my.scenarios = [];
