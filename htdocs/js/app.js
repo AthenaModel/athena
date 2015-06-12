2719f3557c74d956bed583f27ef90038de7c5aec
Will Duquette <William.H.Duquette@jpl.nasa.gov>
2015-06-12 09:29:32 -0700

    Angular web app: Initial Tabs
    * Moved the home and scenario tabs to template/*.html files.
    * Added a radio button column.
    * Hooked it to a model, and verified that we can select it.
    * Next step: add tabs below for the scenario operations.

--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -20,11 +20,23 @@ app.controller('MainTabController', function() {
 });
 
 app.controller('ScenarioListController', ['$http', function($http) {
-    var my = this;
-    my.scenarios = [];
+    var that = this;
+
+    // Select a scenario?
+    this.selectFlag = true;
+    this.allowSelection = function() {
+        if (arguments.length > 0) {
+            this.selectFlag = arguments[0];
+        }
+        return this.selectFlag;
+    };
+    this.selectedCase = "case00";
+
+    // Get the list of scenarios
+    that.scenarios = [];
 
     $http.get('/scenario/index.json').success(function(data) {
-      my.scenarios = data;
+      that.scenarios = data;
     });
 }]);
 
